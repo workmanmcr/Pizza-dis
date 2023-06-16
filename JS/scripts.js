b807dce6476150823e9c087f2b73b79bc8194a1b
workmanmcr <workmanmcr@gmail.com>
2023-06-16 11:30:01 -0700

    test to to see if will return value with both size and toppings

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,33 +1,38 @@
-function Pizza(toppings, size){
-  this.toppings = toppings; 
+function Pizza(toppings, size) {
+  this.toppings = toppings;
   this.size = size;
 }
 
 Pizza.prototype.sizeCost = function() {
-  let baseCost = 0; 
-  if (this.size === "small"){
+  let baseCost = 0;
+  if (this.size === "small") {
     baseCost = 10;
-  }
-    else if (this.size === "medium"){
-      baseCost = 15;
-    } else if (this.size === "large"){
-      baseCost = 20;
-    }
-
-  let sizeCost = baseCost;
-
-  return totalCost;
-}
-Pizza.prototype.toppingCost = function() {
-  
-  if (this.toppings === "cheese") {
-    baseCost = 2;
-  } else if (this.toppings === "peperoni") {
-    baseCost = 4;
-  } else if (this.topping === "suasage"){
-    baseCost =  6; 
+  } else if (this.size === "medium") {
+    baseCost = 15;
+  } else if (this.size === "large") {
+    baseCost = 20;
   }
 
   return baseCost;
-}
-Pizza.prototype.totalCost
+};
+
+Pizza.prototype.toppingCost = function() {
+  let baseCost = 0;
+  if (this.toppings === "cheese") {
+    baseCost = 2;
+  } else if (this.toppings === "pepperoni") {
+    baseCost = 4;
+  } else if (this.toppings === "sausage") {
+    baseCost = 6;
+  }
+
+  return baseCost;
+};
+
+Pizza.prototype.totalCost = function() {
+  let sizeCost = this.sizeCost();
+  let toppingCost = this.toppingCost();
+  let totalCost = sizeCost + toppingCost;
+
+  return totalCost;
+};
